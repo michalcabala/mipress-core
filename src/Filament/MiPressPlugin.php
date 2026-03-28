4fc3cf3b5e958c53f5be614a7431f56a5668f026
michalcabala <michal@cabala.cz>
2026-03-28 15:32:59 +0100

    feat: change Dashboard navigation icon to FontAwesome Solid gauge-high

--- src/Filament/MiPressPlugin.php
+++ src/Filament/MiPressPlugin.php
@@ -32,7 +32,7 @@ class MiPressPlugin implements Plugin
     public function boot(Panel $panel): void
     {
         FilamentIcon::register([
-            PanelsIconAlias::PAGES_DASHBOARD_NAVIGATION_ITEM => 'fal-gauge-high',
+            PanelsIconAlias::PAGES_DASHBOARD_NAVIGATION_ITEM => 'fas-gauge-high',
         ]);
     }
 }
