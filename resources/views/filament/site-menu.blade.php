616e8342749c70a39bf154f95418f8083488026d
michalcabala <michal@cabala.cz>
2026-04-04 15:10:26 +0200

    fix: align site menu icon size and add dropdown header

--- resources/views/filament/site-menu.blade.php
+++ resources/views/filament/site-menu.blade.php
@@ -4,10 +4,14 @@
             type="button"
             class="fi-icon-btn inline-flex items-center gap-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
         >
-            <x-filament::icon icon="heroicon-o-globe-alt" class="h-5 w-5" />
-            <span class="hidden sm:inline text-sm">{{ config('app.name') }}</span>
+            <x-filament::icon icon="heroicon-o-globe-alt" class="h-6 w-6" />
+            <span class="hidden sm:inline text-sm">{{ $siteName }}</span>
         </button>
     </x-slot>
+
+    <x-filament::dropdown.header>
+        {{ $siteName }}
+    </x-filament::dropdown.header>
 
     <x-filament::dropdown.list>
         <x-filament::dropdown.list.item
