e8e48ac12fdaa19c48f5404b1dd64eadf9e27db7
michalcabala <michal@cabala.cz>
2026-04-15 02:51:28 +0200

    Switch publication overview to links

--- resources/views/filament/widgets/entry-publication-status-overview.blade.php
+++ resources/views/filament/widgets/entry-publication-status-overview.blade.php
@@ -1,20 +1,22 @@
 <x-filament-widgets::widget>
     <div class="flex flex-wrap gap-2">
         @foreach ($items as $item)
-            <x-filament::button
-                tag="a"
+            <x-filament::link
                 :href="$item['url']"
                 :color="$item['isActive'] && ($item['key'] === 'all') ? 'primary' : $item['color']"
-                :outlined="! $item['isActive']"
                 :icon="$item['icon']"
                 :badge="$item['count']"
                 :badge-color="$item['isActive'] && ($item['key'] === 'all') ? 'primary' : $item['color']"
                 size="sm"
+                :weight="$item['isActive'] ? 'semibold' : 'medium'"
+                :class="[
+                    'underline decoration-2 underline-offset-4' => $item['isActive'],
+                ]"
                 wire:navigate
                 :aria-current="$item['isActive'] ? 'page' : null"
             >
                 {{ $item['label'] }}
-            </x-filament::button>
+            </x-filament::link>
         @endforeach
     </div>
 </x-filament-widgets::widget>
